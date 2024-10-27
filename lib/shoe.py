a32fce98d50460fa008c1a3acfb77a0c8fbae215
daniel nzioka <dmwinzi50@gmail.com>
2024-10-27 11:18:54 +0300

    submmion

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,36 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand  
+        self.size = size   
+        self._condition = "Used"  
+        
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        if isinstance(brand, str) and 1 <= len(brand) <= 50:  
+            self._brand = brand
+        else:
+            print("brand must be a string")
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    @property
+    def condition(self):
+        return self._condition
+    
+    def cobble(self): 
+        self._condition = "New"  
+        print("Your shoe is as good as new!")  
+
